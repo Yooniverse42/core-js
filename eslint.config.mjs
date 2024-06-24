c20e5ab76686521aa5f3be62f5346278efab84dc
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-06-24 11:47:50 +0900

    0624

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,33 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
+const ignore = {
+  isString:true,
+  isObject:true,
+  isArray:true,
+  isNull:true,
+  getNode:true,
+  insertLast:true,
+  attr:true,
+  css:true,
+  bindEvent:true,
+  addClass:true,
+  removeClass:true,
+}
+
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.browser, 
+        ...globals.node,
+        ...ignore,
+      } 
+    },
+    
+    rules:{
+      "no-unused-vars":'warn'
+    },
+  },
 ];
