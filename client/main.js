8fdc4561abd7af94b187754e6b1f41be6d1c9d59
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-06-25 11:39:32 +0900

    랜덤 주사위 만들기 완료

--- client/main.js
+++ client/main.js
@@ -1,94 +1,115 @@
-import data from './data/data.js';
+
 import { 
-  shake, 
+  diceAnimation, 
   getNode, 
-  addClass,
-  showAlert,
-  getRandom, 
+  getNodes, 
+  attr, 
   insertLast, 
-  removeClass,
-  clearContents,
-  isNumericString,
-  copy} from './lib/index.js';
+  endScroll, 
+  clearContents } from "./lib/index.js";
 
-// [phase-1]
-// 1. 주접 떨기 버튼을 클릭 하는 함수
-//    - 주접 떨기 버튼 가져오기
-//    - 이벤트 연결하기 addEventListener('click')
+// 1. 주사위 애니메이션
 
-// 2. input 값 가져오기
-//    - input.value
-
-// 3. data함수에서 주접 1개 꺼내기
-//    - data(name)
-//    - getRandom()
-
-// 4. pick 항목 랜더링하기
-
-// [phase-2]
-// 1. 아무 값도 입력 받지 못했을 때 예외처리 (콘솔 출력)
-
-const submit = getNode('#submit');
-const nameField = getNode('#nameField');
-const result = getNode('.result');
+const [rollingButton, recordButton, resetButton] = getNodes('.buttonGroup > button');
+const recordListWrapper = getNode('.recordListWrapper')
 
 
+let count = 0;
+let total = 0;
 
-function handleSubmit(e) {
-  e.preventDefault();
+function createItem(diceValue){
+  // 생성 함수
+  const template = `
+    <tr>
+      <td>${++count}</td>
+      <td>${diceValue}</td>
+      <td>${total += diceValue}</td>
+    </tr>
+  `
 
-  const name = nameField.value;
-  const list = data(name);
-  const pick = list[getRandom(list.length)];
-
-
-  if (!name || name.replace(/\s*/g,'') === '') {
-    
-    showAlert('.alert-error','공백은 허용하지 않습니다.')
-    
-    // 왜 갑자기 addClass와 setTimeout(안에 removeClass있음)을 안쓰고 gsap을 쓰냐?
-    // 셋타임아웃은 시간을 보장 받지 못한다고 했죠?
-    // 프라미스를 사용하면 되지만 그 전에 gsap을 이용해보쟈!
-    shake('#nameField').restart();
-
-    return;
-  }
-
-
-  // 숫자만 거르기 위해서 NaN이 맞으면 주접 생성기가 돌아가도록!
-  // 근데 000 === NaN 하면 안됨..자스 특성상
-  // 그래서 isNaN을 사용해야함. 그걸 isNumericString 함수로 만듦
-  if(!isNumericString(name)){
-    
-    showAlert('.alert-error','제대로된 이름을 입력해 주세요.');
-
-    shake('#nameField').restart();
-
-    return;
-  }
-
-  clearContents(result);
-  insertLast(result, pick);
+  return template
 }
 
+function renderRecordItem(){
+  // 기록 함수
+  // const diceValue = getNode('#cube').getAttribute('dice');
+  const diceValue = +attr(getNode('#cube'), 'dice')
 
+  // 1. insertLast 함수 사용
+  // 2. template 전달
+  // 3. diceValue interpolation(보간법) 하기 ex) <td>${diceValue}</td>
+  insertLast('.recordList tbody', createItem(diceValue))
+  endScroll(recordListWrapper)
+}
 
-function handleCopy(){
-  const text = result.textContent;
-
-  if(nameField.value){
-
-    // ~.then() : ~ 했어? 그럼 then 안에 있는 거 실행해!
-    // 근데 then 메서드 앞은 프라미스 구문이어야만 실행 가능
-    // 그래서 copy.js 에서 함수를 return 한거임!
-    copy(text).then(()=>{
-      showAlert('.alert-success','클립보드 복사 완료!');
-    })
-    
-
-  }
+/*function handleRollingDice() {
   
+  let isClicked = false;
+  let stopAnimation;
+
+  return () => {
+    if(!isClicked){
+      console.log('클릭 첫 번째');
+      stopAnimation = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+    }else{
+      console.log('클릭 두 번째');
+      // clearInterval 사용할 땐 setInterval 에서 나온 값을 넣어야 실행이 됨!
+      // 그래서 어떠한 변수에 담아야 하는데, if문 안에서 바로 let이나 const에 담게 되면,
+      // 블록 스코프를 가지게 되서 else문에서는 사용하지 못하게 된다.
+      // 그래서 if..else 문 밖에 let stopAnimation; 을 만들어 둔거임!
+      // 그래서 if문에서도, else 문에서도 같은 변수를 사용할 수 있음.
+      clearInterval(stopAnimation);
+    }
+  
+    isClicked = !isClicked
+  }
 }
 
-submit.addEventListener('click', handleSubmit);
-result.addEventListener('click', handleCopy);
+
+// 클로저! handleRollingDice까지만 쓰면 안쪽 함수 자체가 나오는거기 때문에 
+// handleRollingDice() 까지 써줘야함.
+rollingButton.addEventListener('click', handleRollingDice())*/
+
+// 또는 
+const handleRollingDice = (() => {
+
+  
+  let isClicked = false;
+  let stopAnimation;
+
+  return ()=>{
+    if(!isClicked){
+      stopAnimation  = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+      resetButton.disabled = true;
+    }
+    else{
+      clearInterval(stopAnimation);
+      recordButton.disabled = false;
+      resetButton.disabled = false;
+    }
+    isClicked = !isClicked;
+  }
+})()
+
+// 기록 버튼
+function handleRecord(){
+  recordListWrapper.hidden = false;
+
+  renderRecordItem();
+}
+
+// 초기화 버튼
+function handleReset(){
+  recordListWrapper.hidden = true;
+  clearContents('tbody');
+  count = 0;
+  total = 0;
+}
+
+
+// 이렇게 사용해도 됨. 바로 실행 된거니 호출할 땐 () 안써줘도 됨.
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
