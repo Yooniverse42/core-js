10e8ff7a333989fa8c45a7b0f5f240a063e1607d
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-07-03 13:46:11 +0900

    learn-webComponent

--- client/main.js
+++ client/main.js
@@ -1,26 +1,27 @@
-
-class MyElement extends HTMLElement {
-
+class UserCard extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+    <button>btn</button>`;
+
+    this.button = this.shadowRoot.querySelector('button');
   }
 
-  connectedCallback(){
-
+  connectedCallback() {
+    // this.button.addEventListener('click', () => {
+    //   this.clickMe();
+    // });
+    this.button.addEventListener('click', this.clickMe.bind(this));
   }
 
-  disconnectedCallback(){
-
+  clickMe() {
+    console.log(this);
   }
 }
 
-
-
-customElements.define('c-element', MyElement)
-
+customElements.define('user-card', UserCard);
 
 
 
-
-
-
+// bind는 this를 전달해주는 메서드입니다! 참고!
